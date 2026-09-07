36f8bc483c8b1cd4014ccd89d61033e2c2fe8775
pd <pd@pds-MacBook-Air.local>
2026-09-07 12:17:41 +0300

    Restore Atom button relay test

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,17 @@
 namespace {
 
 constexpr uint8_t kRelayPin = 26;
+constexpr uint8_t kButtonPin = 39;
 constexpr uint8_t kLedPin = 27;
+constexpr uint32_t kDebounceMs = 40;
 
 CRGB statusLed[1];
 
 // The safe startup state leaves COM connected to NC.
 bool relayEnabled = false;
+bool lastRawButtonPressed = false;
+bool stableButtonPressed = false;
+uint32_t lastButtonChangeMs = 0;
 
 void setRelay(bool enabled) {
   digitalWrite(kRelayPin, enabled ? HIGH : LOW);
@@ -25,12 +30,30 @@ void setRelay(bool enabled) {
 void setup() {
   Serial.begin(115200);
   delay(200);
-  Serial.println("camgate safe standby: COM-NC closed");
+  Serial.println("camgate button relay test starting");
 
   pinMode(kRelayPin, OUTPUT);
+  pinMode(kButtonPin, INPUT);
   FastLED.addLeds<SK6812, kLedPin, GRB>(statusLed, 1);
   FastLED.setBrightness(64);
   setRelay(relayEnabled);
 }
 
-void loop() {}
+void loop() {
+  const bool rawButtonPressed = digitalRead(kButtonPin) == LOW;
+
+  if (rawButtonPressed != lastRawButtonPressed) {
+    lastRawButtonPressed = rawButtonPressed;
+    lastButtonChangeMs = millis();
+  }
+
+  if (millis() - lastButtonChangeMs >= kDebounceMs &&
+      rawButtonPressed != stableButtonPressed) {
+    stableButtonPressed = rawButtonPressed;
+
+    if (stableButtonPressed) {
+      relayEnabled = !relayEnabled;
+      setRelay(relayEnabled);
+    }
+  }
+}
